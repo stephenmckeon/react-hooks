99f5495d17273eca8f4ab4acfc38ebca2703982a
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-08-11 11:20:32 -0400

    Implements Edit functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -9,8 +10,11 @@ const App = () => {
     { id: 3, name: 'Will', username: 'WillBill' },
   ]
 
+  const initialFormState = { id: null, name: '', username: '' }
+
   const [users, setUsers] = useState(usersData)
   const [editing, setEditing] = useState(false)
+  const [currentUser, setCurrentUser] = useState(initialFormState)
 
   const addUser = (user) => {
     user.id = users.length + 1
@@ -21,17 +25,42 @@ const App = () => {
     setUsers(users.filter((user) => user.id !== id))
   }
 
+  const editRow = (user) => {
+    setEditing(true)
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)))
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+        {editing ? (
+    <div>
+      <h2>Edit user</h2>
+      <EditUserForm
+        setEditing={setEditing}
+        currentUser={currentUser}
+        updateUser={updateUser}
+      />
+    </div>
+  ) : (
+    <div>
+      <h2>Add user</h2>
+      <AddUserForm addUser={addUser} />
+    </div>
+  )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} removeUser={removeUser} />
+          <UserTable users={users} editRow={editRow} removeUser={removeUser} />
         </div>
       </div>
     </div>
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -16,7 +16,14 @@ const UserTable = (props) => (
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>
-              <button className="button muted-button">Edit</button>
+              <button
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="button muted-button"
+              >
+                Edit
+              </button>
               <button
                 className="button muted-button"
                 onClick={() => props.removeUser(user.id)}
